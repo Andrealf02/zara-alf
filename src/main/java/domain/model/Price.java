a71ccae62432e120a5f6044ed974c26e6022d404
Alf <web.andrea.02@gmail.com>
2024-10-18 21:24:56 +0200

    [WIP] Add Price model

---
+++ src/main/java/domain/model/Price.java
@@ -0,0 +1,91 @@
+package domain.model;
+
+import java.time.LocalDateTime;
+
+
+public class Price {
+    private Long brandId;
+    private LocalDateTime startDate;
+    private LocalDateTime endDate;
+    private Integer priceList;
+    private Long productId;
+    private Integer priority;
+    private Double price;
+    private String currency;
+
+    public Price(Long brandId, LocalDateTime startDate, LocalDateTime endDate, Integer priceList,
+                 Long productId, Integer priority, Double price, String currency) {
+        this.brandId = brandId;
+        this.startDate = startDate;
+        this.endDate = endDate;
+        this.priceList = priceList;
+        this.productId = productId;
+        this.priority = priority;
+        this.price = price;
+        this.currency = currency;
+    }
+
+    public Long getBrandId() {
+        return brandId;
+    }
+
+    public void setBrandId(Long brandId) {
+        this.brandId = brandId;
+    }
+
+    public LocalDateTime getStartDate() {
+        return startDate;
+    }
+
+    public void setStartDate(LocalDateTime startDate) {
+        this.startDate = startDate;
+    }
+
+    public LocalDateTime getEndDate() {
+        return endDate;
+    }
+
+    public void setEndDate(LocalDateTime endDate) {
+        this.endDate = endDate;
+    }
+
+    public Integer getPriceList() {
+        return priceList;
+    }
+
+    public void setPriceList(Integer priceList) {
+        this.priceList = priceList;
+    }
+
+    public Long getProductId() {
+        return productId;
+    }
+
+    public void setProductId(Long productId) {
+        this.productId = productId;
+    }
+
+    public Integer getPriority() {
+        return priority;
+    }
+
+    public void setPriority(Integer priority) {
+        this.priority = priority;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+
+    public String getCurrency() {
+        return currency;
+    }
+
+    public void setCurrency(String currency) {
+        this.currency = currency;
+    }
+}
