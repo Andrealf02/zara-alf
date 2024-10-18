cb71261f428149fd5bb3428a5bde9fa8fb879584
Alf <web.andrea.02@gmail.com>
2024-10-18 20:41:33 +0200

    Firts steps
    -> Create the project
    -> add  graddle
    -> add h2
    [WIP]

---
+++ src/main/java/com/zara/alf/AlfApplication.java
@@ -0,0 +1,13 @@
+package com.zara.alf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlfApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlfApplication.class, args);
+	}
+
+}
